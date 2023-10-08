67a8b6e3c5146850d5460b2027222cf995c05770
cmilsor245 <cmilsor245@g.ecudaand.es>
2023-10-08 23:22:25 +0200

    commit

---
+++ 04-arrays_y_mas/diccionarios/main.py
@@ -0,0 +1,13 @@
+"""
+  @file: main.py
+  @author: Christian Millán Soria
+  @created: 08/10/2023
+  @license: MIT
+  @contact: christiaanmillaan1313@gmail.com
+  @info: basics about dictionaries
+"""
+
+# TODO: (WSL command to execute) -> python3 ./04-arrays_y_mas/diccionarios/main.py
+
+""" ------------- """
+
